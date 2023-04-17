b2ccbf60a1e3bd21f177830757ce36d326b4eba1
Spatricius <p.szmelter@gmail.com>
2023-04-17 21:00:53 +0200

    Update SlackResponseRenderer

--- src/Service/SlackResponseRenderer/GitlabMergeRequestRenderer.php
+++ src/Service/SlackResponseRenderer/GitlabMergeRequestRenderer.php
@@ -15,19 +15,19 @@ class GitlabMergeRequestRenderer implements SlackResponseRendererInterface
     /** @param GitlabMergeRequestParser $slackRequestParser */
     public function resolve(SlackRequestParserInterface $slackRequestParser): string
     {
-        $details = $slackRequestParser->getLazyDetails();
-        $commits = $slackRequestParser->getLazyCommits();
-
-        $commitsText = '';
-        foreach ($commits as $commit) {
-            $commitsText .= "<{$commit['web_url']}|{$commit['short_id']}>";
-            $commitsText .= ", ";
+        $details = $slackRequestParser->getDetails();
+        $filesChanged = [];
+        foreach ($details['diffs'] as $diff) {
+            $filesChanged[$diff['new_path']] = 1;
+            $filesChanged[$diff['old_path']] = 1;
         }
+        $filesChangedText = '- '.implode(",\n- ", array_keys($filesChanged));
 
         $text = <<<TEXT
 MR {$details['title']} ({$details['state']})
 Updated at: {$details['updated_at']}
-Commits: $commitsText
+Files changed:
+$filesChangedText
 TEXT;
 
         return $text;
